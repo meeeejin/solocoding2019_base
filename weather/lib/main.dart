383835f37521c5e3f6adf09a0427c6d984d55c34
meeeejin <meeeeejin@gmail.com>
2019-04-18 16:32:02 +0900

    OWM API를 사용해 실시간 날씨 적용

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -1,10 +1,33 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:weather/widgets/Weather.dart';
 import 'package:weather/widgets/WeatherItem.dart';
+import 'package:weather/models/WeatherData.dart';
+import 'package:weather/models/ForecastData.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  bool isLoading = false;
+  WeatherData weatherData;
+  ForecastData forecastData;
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,14 +50,17 @@ class MyApp extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Weather(),
+                      child: weatherData != null ? Weather(weather: weatherData) : Container(),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: IconButton(
+                      child: isLoading ? CircularProgressIndicator(
+                        strokeWidth: 2.0,
+                        valueColor: new AlwaysStoppedAnimation(Colors.white),
+                      ) : IconButton(
                         icon: new Icon(Icons.refresh),
                         tooltip: 'Refresh',
-                        onPressed: () => null,
+                        onPressed: loadWeather,
                         color: Colors.white,
                       ),
                     ),
@@ -46,11 +72,11 @@ class MyApp extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: Container(
                     height: 200.0,
-                    child: ListView.builder(
-                      itemCount: 10,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) => WeatherItem()
-                    ),
+                    child: forecastData != null ? ListView.builder(
+                        itemCount: forecastData.list.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => WeatherItem(weather: forecastData.list.elementAt(index))
+                    ) : Container(),
                   ),
                 ),
               )
@@ -60,89 +86,32 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  loadWeather() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isLoading = true;
     });
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    final lat = 40.730610;
+    final lon = -73.935242;
+    final weatherResponse = await http.get(
+        'https://api.openweathermap.org/data/2.5/weather?APPID=21894456df795e3f52963983c3e98f01&lat=${lat
+            .toString()}&lon=${lon.toString()}');
+    final forecastResponse = await http.get(
+        'https://api.openweathermap.org/data/2.5/forecast?APPID=21894456df795e3f52963983c3e98f01&lat=${lat
+            .toString()}&lon=${lon.toString()}');
+
+    if (weatherResponse.statusCode == 200 &&
+        forecastResponse.statusCode == 200) {
+      return setState(() {
+        weatherData = new WeatherData.fromJson(jsonDecode(weatherResponse.body));
+        forecastData = new ForecastData.fromJson(jsonDecode(forecastResponse.body));
+        isLoading = false;
+      });
+    }
+
+    setState(() {
+      isLoading = false;
+    });
   }
 }
