b5210d35a95697b436c802e5918b448ef113bd13
meeeejin <meeeeejin@gmail.com>
2019-04-18 15:53:18 +0900

    파일 분리

--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather/widgets/Weather.dart';
+import 'package:weather/widgets/WeatherItem.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,17 +27,8 @@ class MyApp extends StatelessWidget {
                   children: <Widget>[
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text('New York', style: new TextStyle(color: Colors.white)),
-                          Text('Rain', style: new TextStyle(color: Colors.white, fontSize: 32.0)),
-                          Text('72°F',  style: new TextStyle(color: Colors.white)),
-                          Image.network('https://openweathermap.org/img/w/01d.png'),
-                          Text('Jun 28, 2018', style: new TextStyle(color: Colors.white)),
-                          Text('18:30', style: new TextStyle(color: Colors.white)),
-                        ],
-                      ),
-                    ),        
+                      child: Weather(),
+                    ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: IconButton(
@@ -56,22 +49,7 @@ class MyApp extends StatelessWidget {
                     child: ListView.builder(
                       itemCount: 10,
                       scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) => Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text('New York', style: new TextStyle(color: Colors.black)),
-                              Text('Rain', style: new TextStyle(color: Colors.black, fontSize: 24.0)),
-                              Text('72°F',  style: new TextStyle(color: Colors.black)),
-                              Image.network('https://openweathermap.org/img/w/01d.png'),
-                              Text('Jun 28, 2018', style: new TextStyle(color: Colors.black)),
-                              Text('18:30', style: new TextStyle(color: Colors.black)),
-                            ],
-                          ),
-                        ),
-                      )
+                      itemBuilder: (context, index) => WeatherItem()
                     ),
                   ),
                 ),
